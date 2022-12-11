7a286ec63a56ee01bdb19650e182b9ad23d27402
Hicster Enthusiast <hicster.enthusiast@gmail.com>
2022-12-11 11:34:32 -0500

    feat: Add explorer window

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ splashScreen.addEventListener('click',() => {
 let wTypeError = 0
 let iTypeError = 0
 
-let wTypeB = 0
-let iTypeB = 0
+let wTypeExplorer = 0
+let iTypeExplorer = 0
 
 // References and abstracions to DOM objects
 let windows = new Array
@@ -19,8 +19,8 @@ let bar = document.querySelector('.task-bar')
 let iconGrid = document.querySelector('.icons')
 let icons = document.querySelectorAll('.icon')
 
-iconGrid.append(createIcon("error_oom"))
-iconGrid.append(createIcon("type_b"))
+iconGrid.append(createIcon('error_oom'))
+iconGrid.append(createIcon('explorer'))
 
 
 // TODO
@@ -72,8 +72,48 @@ function createWindow(winType) {
         pTitle.innerText = "An error has occured"
 
 
-    } else if (winType == 'type_b') {
-        winCounter = ++wTypeB
+    } else if (winType == 'explorer') {
+        winCounter = ++wTypeExplorer
+        window.style.width = '500px'
+        window.style.height = '300px'
+
+        let navbar = document.createElement('div')
+        navbar.classList.add('navbar')
+
+        let navtext = document.createElement('p')
+        navtext.innerText = '~'
+        navbar.append(navtext)
+
+        let folders = document.createElement('div')
+        folders.style.position = 'static'
+        folders.classList.add('icons')
+
+        let backBtn = document.createElement('button')
+        navbar.append(backBtn)
+        backBtn.addEventListener('click', () => {
+            let arr = navtext.innerText.split('/')
+            if (arr.length > 1) {
+                arr.pop(arr.length-1)
+                navtext.innerText = arr.join('/')
+                let newFolders = getFoldersAt(arr[arr.length-1])
+                folders.innerText = ''
+
+
+                newFolders.forEach(folder => {
+                    folders.append(folder)
+                });
+            }
+        })
+
+        windowContent.append(navbar)
+
+
+        let arrFolders = getFoldersAt('home')
+        arrFolders.forEach(folder => {
+            folders.append(folder)
+        });
+
+        windowContent.append(folders)
     }
     window.id = 'win-' + winType + '-' + winCounter 
     if (pTitle.innerText == '') { pTitle.innerText = window.id }
@@ -151,6 +191,37 @@ function createError(errType) {
 
     return constructedContent
 }
+function getFoldersAt(location) {
+    let folders = new Array
+    if (location == '~') { location = 'home' }
+
+    switch (location) {
+        case 'home':
+            folders.push(createIcon('folder_music'))
+            folders.push(createIcon('folder_pictures'))
+            folders.push(createIcon('folder_documents'))
+            folders.push(createIcon('folder_.local'))         
+            break;
+        case 'music':
+            folders.push(createIcon('folder_test2'))
+            folders.push(createIcon('folder_test3'))
+            break;
+        case 'documents':
+            folders.push(createIcon('folder_investigations'))
+            break;
+        case 'investigations':
+            folders.push(createIcon('folder_00'))
+            folders.push(createIcon('folder_01'))
+            folders.push(createIcon('folder_02'))
+            folders.push(createIcon('folder_03'))
+            folders.push(createIcon('folder_04'))
+            break;
+        default:
+            break;
+    }
+
+    return folders
+}
 function drawWindows(win, x, y) {
     win.style.top = y + 'px'
     win.style.left = x + 'px'
@@ -167,40 +238,57 @@ function createIcon(winType) {
 
     if (winType.includes('error')) {
         iCounter = ++iTypeError
-    } else if (winType == 'type_b') {
-        iCounter = ++iTypeB
+    } else if (winType == 'explorer') {
+        iCounter = ++iTypeExplorer
     }
     icon.id = 'icon-' + winType + '-' + iCounter
     
     let p = document.createElement('p')
-    p.innerText = winType
+    p.innerText = winType.charAt(0).toUpperCase() + winType.slice(1).replace('_', ' ')
     let img = document.createElement('img')
 
     icon.append(img)
     icon.append(p)
 
-    icon.addEventListener('dblclick', () => {
-        document.body.style.cursor = 'wait'
-        icon.style.cursor = 'wait'
-        setTimeout(() => {
-            if (windows.length == 10){
-                let win = createWindow("error_oom")
-                drawWindows(win, 100, 100)
+    if (winType.includes('folder')) {
+        let destination = winType.split('_')[1]
+        p.innerText = destination.charAt(0).toUpperCase() + destination.slice(1)
+        icon.addEventListener('dblclick', () => {
+            let parentContainer = icon.parentElement
+            let navtext = parentContainer.parentElement.firstChild.firstChild
+            parentContainer.innerText = ''
+            navtext.innerText = navtext.innerText + '/' + destination
+
+            let foldersAtDestination = getFoldersAt(destination)
+
+            foldersAtDestination.forEach(folder => {
+                parentContainer.append(folder)
+            });
+        })
+    } else {
+        icon.addEventListener('dblclick', () => {
+            document.body.style.cursor = 'wait'
+            icon.style.cursor = 'wait'
+            setTimeout(() => {
+                if (windows.length == 10){
+                    let win = createWindow("error_oom")
+                    drawWindows(win, 100, 100)
+                    document.body.style.cursor = "auto" 
+                    return
+                } else if (windows.length > 10){
+                    document.body.style.cursor = "auto" 
+                    return
+                }
+                if (iteration>=(initialValue*4)) { iteration = initialValue}
+                let win = createWindow(icon.id.split("-")[1])
+                drawWindows(win, 100 + (15*(iteration%initialValue)),
+                                100 + (15*(iteration%initialValue)) + (30*(Math.ceil((iteration+1)/initialValue))-2))
                 document.body.style.cursor = "auto" 
-                return
-            } else if (windows.length > 10){
-                document.body.style.cursor = "auto" 
-                return
-            }
-            if (iteration>=(initialValue*4)) { iteration = initialValue}
-            let win = createWindow(icon.id.split("-")[1])
-            drawWindows(win, 100 + (15*(iteration%initialValue)),
-                            100 + (15*(iteration%initialValue)) + (30*(Math.ceil((iteration+1)/initialValue))-2))
-            document.body.style.cursor = "auto" 
-            icon.style.cursor = 'pointer'
-            ++iteration
-        }, 400)
-    })
+                icon.style.cursor = 'pointer'
+                ++iteration
+            }, 400)
+        })
+    }
 
     return icon
 }
